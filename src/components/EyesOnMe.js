9773324ccf6827f6d2f6a33f7706debcaee8312c
Adam DePaolo <amdepaolo@gmail.com>
2022-06-13 19:17:29 -0400

    commit tree 019d9c9712e60ccff7bf3bf2be7169d40fa83760

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    function eyesFocus(){
+        console.log("Good!")
+    }
+    function eyesBlur(){
+        console.log('Hey! Eyes on me!')
+    }
+    return(
+        <button onFocus={eyesFocus} onBlur={eyesBlur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad(){
+    function keypadChange(){
+        console.log('Entering password...')
+    }
+    return(
+        <input type='password' onChange={keypadChange} />
+    )
+}
+
+export default Keypad
